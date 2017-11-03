569989ade5350f68b2b3eb1f9b59acacabde3ca1
Kenneth Chow <kennethchow74@gmail.com>
2017-11-03 12:54:48 -0700

    post container list styles and select field color changes.

--- client/main.js
+++ client/main.js
@@ -2,11 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
 import App from "../imports/ui/containers/App";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import "./main.css";
 
 Meteor.startup(() => {
-  ReactDOM.render(
-    <App />,
-    document.getElementById("root")
-  );
+  ReactDOM.render(<App />, document.getElementById("root"));
 });
