8a5e5e88de40488bfcb688e5d40c70292d3cbdbb
mr-moto <khash615@gmail.com>
2017-11-03 12:36:33 -0700

    Removed unnecessary code from Main.js

--- client/main.js
+++ client/main.js
@@ -2,18 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom";
 import { Meteor } from "meteor/meteor";
 import App from "../imports/ui/containers/App";
-import { Routes } from "../imports/ui/routes/index";
-import { BrowserRouter as Router } from "react-router";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 import "./main.css";
 
 Meteor.startup(() => {
   ReactDOM.render(
-    // <MuiThemeProvider>
-    // <Router>
     <App />,
-    // </Router>,
-    // </MuiThemeProvider>,
     document.getElementById("root")
   );
 });
