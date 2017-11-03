a43aa857cf92083a3e0dee5f7f2725b0a30d8285
mr-moto <khash615@gmail.com>
2017-11-03 10:48:14 -0700

    Added Header Component and basic structure.

---
+++ imports/ui/containers/Header/Header.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+    state = {  }
+    render() {
+        return (
+            <div>THIS IS THE HEADDERRRRRRRRRRRR!!!!!</div>
+        );
+    }
+}
+
+export default Header;
