a05fb75e58537181a3d344460dc1952edb48d307
mr-moto <khash615@gmail.com>
2017-11-03 12:31:58 -0700

    Added Material-Ui components and functionality to Header

--- imports/ui/containers/Header/Header.js
+++ imports/ui/containers/Header/Header.js
@@ -1,10 +1,55 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
+import { AppBar, IconButton, FontIcon, Drawer, MenuItem } from "material-ui";
+import { Link } from "react-router-dom";
+
+import PropTypes from "prop-types";
+
+function handleTouchTapMenu() {
+    console.log("burger lcickeddddd");
+}
 
 class Header extends Component {
-    state = {  }
+    constructor(props) {
+        super(props);
+        this.state = { open: false };
+    }
+    handleMenuToggle = () => this.setState({ open: !this.state.open });
+    handleClose = () => this.setState({ open: false });
     render() {
         return (
-            <div>THIS IS THE HEADDERRRRRRRRRRRR!!!!!</div>
+            <div>
+                <AppBar
+                    title="Title"
+                    onLeftIconButtonTouchTap={this.handleMenuToggle}
+                />
+                <Drawer
+                    docked={false}
+                    width={200}
+                    open={this.state.open}
+                    onRequestChange={open => this.setState({ open })}
+                >
+                    <MenuItem onClick={this.handleClose}>
+                        <Link to="/" className="home">
+                            Home
+                        </Link>
+                    </MenuItem>
+                    <MenuItem onClick={this.handleClose}>
+                        <Link to="/profile" className="profile">
+                            Profile
+                        </Link>
+                    </MenuItem>
+                    <MenuItem onClick={this.handleClose}>
+                        <Link to="/profile/settings" className="settings">
+                            Settings
+                        </Link>
+                    </MenuItem>
+                    <MenuItem onClick={this.handleClose}>
+                        <Link to="/login" className="logout">
+                            Logout
+                        </Link>
+                    </MenuItem>
+                </Drawer>
+            </div>
         );
     }
 }
