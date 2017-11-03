64a0b278d47be1ad132c2de76e6bf0333912b745
DanielKell <danielkell.ubc@gmail.com>
2017-11-02 22:38:48 -0700

    Added tab toggling

--- imports/ui/containers/Profile/Profile.js
+++ imports/ui/containers/Profile/Profile.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Meteor } from 'meteor/meteor';
 import {Card, CardTitle} from 'material-ui/Card';
+import {Tabs, Tab} from 'material-ui/Tabs';
 
 import './styles.css';
 
@@ -13,15 +14,42 @@ export const Profile = () => {
                         <div className="profile-name">
                             <CardTitle title="title" subtitle="subtitle" />
                         </div>
-                        <div className="profile-item-data">
-                            <CardTitle title="2" subtitle="Items shared" />
-                            <CardTitle title="2" subtitle="Items borrowed" />
+                        <div className="compliments-given">
+                            <CardTitle title="Compliments given: " subtitle="2" />
                         </div>
-                        {/*<div className="profile-gravatar">
-                            <Gravatar size={150} email={singleUserData.email} className="gravatar-image"/>
-                        </div>*/}
+                        <div className="compliments-received">
+                            <CardTitle title="Compliments given: " subtitle="3" />
+                        </div>
+                        <div className="user-image-and-badge">
+                            <div className="user-image">
+                                User image
+                            </div>
+                            <div className="badge-overlay">
+                            </div>
+                        </div>
                     </div>
                 </Card>
+            </div>
+
+        <div className="profile-compliments">
+             <Tabs>
+                <Tab label="Item One" >
+                    <div>
+                        <h2 className="tab">Tab One</h2>
+                        <p>
+                        Compliments received!
+                        </p>
+                    </div>
+                    </Tab>
+                    <Tab label="Item Two" >
+                    <div>
+                        <h2 className="tab">Tab Two</h2>
+                        <p>
+                        Compliments sent!
+                        </p>
+                    </div>
+                    </Tab>
+                </Tabs>
             </div>
         </div>
     )
