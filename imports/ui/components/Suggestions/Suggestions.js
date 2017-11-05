4165792a097b581f7cdbeac646eed7b2a11bba5d
DanielKell <danielkell.ubc@gmail.com>
2017-11-05 10:42:19 -0800

    Added simple cycling text

--- imports/ui/components/Suggestions/Suggestions.js
+++ imports/ui/components/Suggestions/Suggestions.js
@@ -2,10 +2,38 @@ import React from 'react';
 
 import './styles.css';
 
+var splittedText = ["Hello", "World", "How", "Are", "You", "Today"];
+
+function loopThroughSplittedText(splittedText) {
+    for (var i = 0; i < splittedText.length; i++) {
+        // for each iteration console.log a word
+        // and make a pause after it
+        (function (i) {
+            setTimeout(function () {
+                document.getElementById('generated-suggestion').innerHTML = "";
+                document.getElementById('generated-suggestion').innerHTML += splittedText[i];
+                //console.log(splittedText[i]);
+            }, 3000 * i);
+        })(i);
+    };
+}
+loopThroughSplittedText(splittedText);
+
+
+
 const Suggestions = () => {
     return(
         <div className="suggestions">
-            Suggestions
+            <div className="suggestions-box">
+                <div>
+                    Lost for words? 
+                </div>
+                <div id="generated-suggestion">
+                    {loopThroughSplittedText(splittedText)}
+                </div>
+                <div>
+                </div>
+            </div>
         </div>
     );
 }
