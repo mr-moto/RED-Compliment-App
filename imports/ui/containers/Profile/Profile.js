b08555c2aa8789c90f06fde2f767a996b6be90c2
DanielKell <danielkell.ubc@gmail.com>
2017-11-06 13:45:04 -0800

    Defined sent and received posts based on active user

--- imports/ui/containers/Profile/Profile.js
+++ imports/ui/containers/Profile/Profile.js
@@ -26,82 +26,97 @@ const styles = {
   }
 };
 
-export const Profile = () => {
-    return (
-        <div> 
-            <div className="profile-card">
-                <Card>
-                    <div className="card-structure">
-                        <div className="card-left">
-                            <div className="profile-name">
-                                <CardTitle 
-                                    titleStyle={styles.name} 
-                                    title="Mandi Wise" 
-                                />
-                            </div>
-                            <div className="stats">
-                                <div className="compliments-given">
-                                    <CardTitle 
-                                        titleStyle={styles.stats} 
-                                        title="Compliments received: " 
-                                        subtitle="2" 
-                                        subtitleStyle={styles.subtitle}
-                                    />
-                                </div>
-                                <div className="compliments-received">
-                                    <CardTitle 
-                                        titleStyle={styles.stats} 
-                                        title="Compliments given: " 
-                                        subtitle="3" 
-                                        subtitleStyle={styles.subtitle}
-                                    />
-                                </div>
-                            </div>
-                        </div>
-                        <div className="card-right">
-                            <div className="user-image-and-badge">
-                                <div className="user-image">
-                                    User image
-                                </div>
-                                <div className="badge-overlay">
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </Card>
-            </div>
+export const Profile = ({currentUser, posts}) => {
 
-            <div className="profile-compliments">
-                <div className="tab-container">
-                    <Tabs 
-                        tabItemContainerStyle={{backgroundColor: 'darkgray'}} 
-                        inkBarStyle={{background: 'red'}}
-                    >
-                        <Tab 
-                            style={styles.tab} 
-                            label="Compliments Received"
-                        >
-                            <div>
-                                <h2 className="tab">You're popular! You've received X compliments: </h2>
-                                <p>
-                                Here are the compliments
-                                </p>
+    if (posts.length > 0 && currentUser) {
+
+        //All sent posts from this user
+        const sentPosts = posts.filter(post => post.from._id === currentUser._id)
+
+        //All received posts from this user
+        const receivedPosts = posts.filter(post => post.to._id === currentUser._id)
+        console.log(receivedPosts);
+    }
+    
+    if (currentUser) {
+
+        return (
+            <div> 
+                <div className="profile-card">
+                    <Card>
+                        <div className="card-structure">
+                            <div className="card-left">
+                                <div className="profile-name">
+                                    <CardTitle 
+                                        titleStyle={styles.name} 
+                                        title={`${currentUser.profile.firstName} ${currentUser.profile.lastName}`}
+                                    />
+                                </div>
+                                <div className="stats">
+                                    <div className="compliments-given">
+                                        <CardTitle 
+                                            titleStyle={styles.stats} 
+                                            title="Compliments received: " 
+                                            subtitle="2" 
+                                            subtitleStyle={styles.subtitle}
+                                        />
+                                    </div>
+                                    <div className="compliments-received">
+                                        <CardTitle 
+                                            titleStyle={styles.stats} 
+                                            title="Compliments given: " 
+                                            subtitle="3" 
+                                            subtitleStyle={styles.subtitle}
+                                        />
+                                    </div>
+                                </div>
                             </div>
-                            </Tab>
+                            <div className="card-right">
+                                <div className="user-image-and-badge">
+                                    <div className="user-image">
+                                        User image
+                                    </div>
+                                    <div className="badge-overlay">
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Card>
+                </div>
+
+                <div className="profile-compliments">
+                    <div className="tab-container">
+                        <Tabs 
+                            tabItemContainerStyle={{backgroundColor: 'darkgray'}} 
+                            inkBarStyle={{background: 'red'}}
+                        >
                             <Tab 
                                 style={styles.tab} 
-                                label="Compliments Sent" 
+                                label="Compliments Received"
                             >
-                            <div>
-                                <h2 className="tab">So generous. Much kind. You've sent X compliments:</h2>
-                                <p>
-                                Here are the compliments
-                                </p>
-                            </div>
-                        </Tab>
-                    </Tabs>
+                                <div>
+                                    <h2 className="tab">You're popular! You've received X compliments: </h2>
+                                    <p>
+                                    Here are the compliments
+                                    </p>
+                                </div>
+                                </Tab>
+                                <Tab 
+                                    style={styles.tab} 
+                                    label="Compliments Sent" 
+                                >
+                                <div>
+                                    <h2 className="tab">So generous. Much kind. You've sent X compliments:</h2>
+                                    <p>
+                                    Here are the compliments
+                                    </p>
+                                </div>
+                            </Tab>
+                        </Tabs>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
+    return <div> Is Loading... </div>;
 } 
