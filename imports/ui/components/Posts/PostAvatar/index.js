1f90f8b13373b1435972a8131bd88109951edaca
Kenneth Chow <kennethchow74@gmail.com>
2017-11-07 14:12:20 -0800

    slight style adjustments.

--- imports/ui/components/Posts/PostAvatar/index.js
+++ imports/ui/components/Posts/PostAvatar/index.js
@@ -4,12 +4,7 @@ import Avatar from "material-ui/Avatar";
 const PostAvatar = ({ src, avatarSize }) => {
   return (
     <div className="post-avatar-container">
-      <Avatar
-        className="post-avatar"
-        src={src}
-        size={avatarSize}
-        style={{ marginRight: "0.5rem" }}
-      />
+      <Avatar className="post-avatar" src={src} size={avatarSize} />
     </div>
   );
 };
