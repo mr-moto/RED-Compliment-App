92878ddb7b483e60590409efa22c37d1f47a6558
mr-moto <khash615@gmail.com>
2017-11-07 13:42:44 -0800

    implemented text loop for quotes on the login page

--- imports/ui/containers/Login/index.js
+++ imports/ui/containers/Login/index.js
@@ -1,17 +1,18 @@
 import React, { Component } from "react";
 import { withTracker } from "meteor/react-meteor-data";
 import { Redirect } from "react-router-dom";
+import TextLoop from "react-text-loop";
 
 import { SignIn } from "../../components/SignIn";
 
 import "./styles.css";
 
-const login = (e) => {
+const login = e => {
     e.preventDefault();
     let email = e.target.email.value;
     let password = e.target.password.value;
     Meteor.loginWithPassword(email, password);
-}
+};
 class LoginContainer extends Component {
     state = {};
     render() {
@@ -21,7 +22,24 @@ class LoginContainer extends Component {
         } else {
             return (
                 <div className="loginContainer">
-                    <SignIn submit={login} label1={'Register'} label2={'Sign In'} link={'/register'} register={false} />
+                    <SignIn
+                        submit={login}
+                        label1={"Register"}
+                        label2={"Sign In"}
+                        link={"/register"}
+                        register={false}
+                    />
+                    <div className="quotesContainer">
+                        <h2>
+                            <TextLoop>
+                                <span>ShamWOWOWOW</span>
+                                <span>This is quote</span>
+                                <span>kickass quote</span>
+                                <span>and and and</span>
+                                <span>pewpewpew 💩</span>
+                            </TextLoop> and something else.
+                        </h2>
+                    </div>
                 </div>
             );
         }
