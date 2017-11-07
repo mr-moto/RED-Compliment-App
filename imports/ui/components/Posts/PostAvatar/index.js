e150f42afa8dc03817cd3bbd5f4050e51dc1a53f
DanielKell <danielkell.ubc@gmail.com>
2017-11-06 17:02:50 -0800

    Adjusted image sizes for Profile

--- imports/ui/components/Posts/PostAvatar/index.js
+++ imports/ui/components/Posts/PostAvatar/index.js
@@ -1,13 +1,13 @@
 import React from "react";
 import Avatar from "material-ui/Avatar";
 
-const PostAvatar = ({ src }) => {
+const PostAvatar = ({ src, avatarSize }) => {
   return (
     <div className="post-avatar-container">
       <Avatar
         className="post-avatar"
         src={src}
-        size={50}
+        size={avatarSize}
         style={{ marginRight: "0.5rem" }}
       />
     </div>
