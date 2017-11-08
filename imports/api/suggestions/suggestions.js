2fa7ab2478ea3fae1f18bb1c30fb7c76e24896c9
DanielKell <danielkell.ubc@gmail.com>
2017-11-07 21:18:51 -0800

    Added suggestions into db

--- imports/api/suggestions/suggestions.js
+++ imports/api/suggestions/suggestions.js
@@ -3,3 +3,9 @@ import { Mongo } from "meteor/mongo";
 export const Suggestions = new Mongo.Collection("suggestions");
 
 // SUGGESTIONS METHODS HERE
+
+if (Meteor.isServer) {
+  Meteor.publish("suggestions", function suggestionsPublication() {
+    return Suggestions.find({});
+  });
+}
