2a4cc47824fa0f66df603507b150f6fea545fb87
Kenneth Chow <kennethchow74@gmail.com>
2017-11-11 18:00:45 -0800

    added date insert into api.

--- imports/api/posts/posts.js
+++ imports/api/posts/posts.js
@@ -5,67 +5,78 @@ import ShareConfirmation from "../../ui/components/Share/ShareConfirmation";
 export const Posts = new Mongo.Collection("posts");
 
 if (Meteor.isServer) {
-    Meteor.publish("posts", function postsPublication() {
-        return Posts.find({});
-    });
+  Meteor.publish("posts", function postsPublication() {
+    return Posts.find({});
+  });
 }
 
 // POSTS METHODS HERE
 Meteor.methods({
-    "posts.removeUserId"(post, collection) {
-        Posts.update(
-            { _id: `${post}` },
-            { $pull: { [collection]: `${Meteor.user()._id}` } }
-        );
-    },
-    "posts.addUserId"(post, collection) {
-        Posts.update(
-            { _id: `${post}` },
-            { $addToSet: { [collection]: `${Meteor.user()._id}` } }
-        );
-    },
-    "posts.addCompliment"(to, body, from, users) {
-        // set some rules for to
-        // search for matches and reassign to with obj
-        to = users.find(user => {
-            name = `${user.profile.firstName} ${user.profile.lastName}`;
-            if (to === name) {
-                return name;
-            } else {
-                return undefined;
-            }
-        });
+  "posts.removeUserId"(post, collection) {
+    Posts.update(
+      { _id: `${post}` },
+      { $pull: { [collection]: `${Meteor.user()._id}` } }
+    );
+  },
+  "posts.addUserId"(post, collection) {
+    Posts.update(
+      { _id: `${post}` },
+      { $addToSet: { [collection]: `${Meteor.user()._id}` } }
+    );
+  },
+  "posts.addCompliment"(to, body, from, users) {
+    const dateArr = [];
+    const tzOffset = new Date().getTimezoneOffset() * 60000; // offset in milliseconds
+    const localTime = `${new Date(Date.now() - tzOffset)
+      .toUTCString()
+      .slice(5, -13)
+      .split(" ")
+      .forEach(elem => dateArr.push(elem))}`;
 
-        if (to !== undefined) {
-            toID = to._id;
-        } else {
-            return console.log("No user with that name.");
-        }
+    const formattedDate = `${dateArr[1]} ${dateArr[0]} ${dateArr[2]}`;
 
-        if (
-            toID !== undefined &&
-            body.length > 0 &&
-            `${from._id}` === `${Meteor.user()._id}` &&
-            `${toID}` !== `${Meteor.user()._id}`
-        ) {
-            Posts.insert({
-                to,
-                body,
-                from,
-                upvotes: [],
-                sarcasm: []
-            });
+    // set some rules for to
+    // search for matches and reassign to with obj
+    to = users.find(user => {
+      name = `${user.profile.firstName} ${user.profile.lastName}`;
+      if (to === name) {
+        return name;
+      } else {
+        return undefined;
+      }
+    });
 
-            return true;
-        } else {
-            return false;
-        }
-    },
-    "posts.updatePostsImage"(userId, imageUrl) {
-        Posts.update(
-            { "from._id": `${userId}` },
-            { $set: { "from.profile.photo": `${imageUrl}` } },
-            { multi: true }
-        );
+    if (to !== undefined) {
+      toID = to._id;
+    } else {
+      return console.log("No user with that name.");
     }
+
+    if (
+      toID !== undefined &&
+      body.length > 0 &&
+      `${from._id}` === `${Meteor.user()._id}` &&
+      `${toID}` !== `${Meteor.user()._id}`
+    ) {
+      Posts.insert({
+        to,
+        body,
+        from,
+        upvotes: [],
+        sarcasm: [],
+        dateCreated: formattedDate
+      });
+
+      return true;
+    } else {
+      return false;
+    }
+  },
+  "posts.updatePostsImage"(userId, imageUrl) {
+    Posts.update(
+      { "from._id": `${userId}` },
+      { $set: { "from.profile.photo": `${imageUrl}` } },
+      { multi: true }
+    );
+  }
 });
