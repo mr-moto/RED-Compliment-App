1bc448589b7ca46283547f2a8df40d15eb022367
Kenneth Chow <kennethchow74@gmail.com>
2017-11-13 21:45:31 -0800

    added padding to annoymous toggle button.

--- imports/ui/components/Share/ShareForm/index.js
+++ imports/ui/components/Share/ShareForm/index.js
@@ -7,106 +7,107 @@ import Suggestions from "../../../components/Suggestions";
 import "./styles";
 
 const ShareForm = ({
-    shareIsExpanded,
-    showShareForm,
-    addCompliment,
-    toValue,
-    bodyValue,
-    handleToChange,
-    handleBodyChange,
-    suggestionsList,
-    toggle
+  shareIsExpanded,
+  showShareForm,
+  addCompliment,
+  toValue,
+  bodyValue,
+  handleToChange,
+  handleBodyChange,
+  suggestionsList,
+  toggle
 }) => {
-    return (
-        <div>
-            <Paper
-                id="share-container"
-                className={`share-container ${shareIsExpanded
-                    ? "share-clicked"
-                    : "share-collapsed"}`}
-                style={{ background: "#3c3737" }}
-            >
-                <Paper
-                    className="share-bar"
-                    zDepth={1}
-                    rounded={false}
-                    style={{ background: "rgb(115, 115, 115)" }}
-                    onClick={() => {
-                        showShareForm();
-                    }}
-                >
-                    <span className="share-button">+</span>
-                </Paper>
-                <form
-                    onSubmit={addCompliment}
-                    id="compliment-form"
-                    className={shareIsExpanded ? "" : `hide`}
-                >
-                    <div className="share-fields">
-                        <TextField
-                            value={toValue}
-                            className="share-to"
-                            id="share-to"
-                            name="shareTo"
-                            hintText="To."
-                            onChange={handleToChange}
-                            underlineFocusStyle={{
-                                borderColor: "#ed4242"
-                            }}
-                            hintStyle={{
-                                color: "rgba(255, 255, 255, 0.5)"
-                            }}
-                            inputStyle={{
-                                color: "#fff"
-                            }}
-                            autoComplete="off"
-                        />
-                        <TextField
-                            value={bodyValue}
-                            onChange={handleBodyChange}
-                            className="share-body"
-                            id="share-body"
-                            name="shareBody"
-                            hintText="Compliment"
-                            underlineFocusStyle={{
-                                borderColor: "#ed4242"
-                            }}
-                            hintStyle={{
-                                color: "rgba(255, 255, 255, 0.5)"
-                            }}
-                            inputStyle={{
-                                color: "#fff"
-                            }}
-                            textareaStyle={{
-                                color: "#fff"
-                            }}
-                            multiLine={true}
-                            autoComplete="off"
-                        />
-                        <Toggle
-                            label="Post as Anonymous"
-                            labelStyle={{
-                                color: "white"
-                            }}
-                            onToggle={toggle}
-                        />
-                        <RaisedButton
-                            label="Submit"
-                            type="submit"
-                            style={{ margin: 12 }}
-                            labelColor={"#fff"}
-                            overlayStyle={{ background: "#ed4242" }}
-                        />
-                    </div>
-                    <Suggestions suggestionsList={suggestionsList} />
-                </form>
-            </Paper>
-        </div>
-    );
+  return (
+    <div>
+      <Paper
+        id="share-container"
+        className={`share-container ${shareIsExpanded
+          ? "share-clicked"
+          : "share-collapsed"}`}
+        style={{ background: "#3c3737" }}
+      >
+        <Paper
+          className="share-bar"
+          zDepth={1}
+          rounded={false}
+          style={{ background: "rgb(115, 115, 115)" }}
+          onClick={() => {
+            showShareForm();
+          }}
+        >
+          <span className="share-button">+</span>
+        </Paper>
+        <form
+          onSubmit={addCompliment}
+          id="compliment-form"
+          className={shareIsExpanded ? "" : `hide`}
+        >
+          <div className="share-fields">
+            <TextField
+              value={toValue}
+              className="share-to"
+              id="share-to"
+              name="shareTo"
+              hintText="To."
+              onChange={handleToChange}
+              underlineFocusStyle={{
+                borderColor: "#ed4242"
+              }}
+              hintStyle={{
+                color: "rgba(255, 255, 255, 0.5)"
+              }}
+              inputStyle={{
+                color: "#fff"
+              }}
+              autoComplete="off"
+            />
+            <TextField
+              value={bodyValue}
+              onChange={handleBodyChange}
+              className="share-body"
+              id="share-body"
+              name="shareBody"
+              hintText="Compliment"
+              underlineFocusStyle={{
+                borderColor: "#ed4242"
+              }}
+              hintStyle={{
+                color: "rgba(255, 255, 255, 0.5)"
+              }}
+              inputStyle={{
+                color: "#fff"
+              }}
+              textareaStyle={{
+                color: "#fff"
+              }}
+              multiLine={true}
+              autoComplete="off"
+            />
+            <Toggle
+              label="Post as Anonymous"
+              labelStyle={{
+                color: "white"
+              }}
+              onToggle={toggle}
+              style={{ padding: "1rem" }}
+            />
+            <RaisedButton
+              label="Submit"
+              type="submit"
+              style={{ margin: 12 }}
+              labelColor={"#fff"}
+              overlayStyle={{ background: "#ed4242" }}
+            />
+          </div>
+          <Suggestions suggestionsList={suggestionsList} />
+        </form>
+      </Paper>
+    </div>
+  );
 };
 
 ShareForm.propTypes = {
-    suggestionsList: PropTypes.object
+  suggestionsList: PropTypes.object
 };
 
 export default ShareForm;
